766228598d58e5385c92de537ca6881c88cbbaaf
sangyeopkim <yeop123456@gmail.com>
2017-02-28 16:41:18 +0900

    Facebook - login_facebook 구현

--- facebook/django_app/member/urls.py
+++ facebook/django_app/member/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 app_name = 'member'
 urlpatterns = [
-    url(r'^login/', views.login_fbv, name='login')
+    url(r'^login/', views.login_fbv, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook'),
 ]
