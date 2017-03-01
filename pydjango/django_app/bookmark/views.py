ccc23381d9d7988f969f205c7b663e7a84526d77
sangyeopkim <yeop123456@gmail.com>
2017-03-01 15:51:10 +0900

    Generic View 를 사용하여 BookmarkLV(ListView), BookmarkDV(DetailView) 클래스 생성

--- pydjango/django_app/bookmark/views.py
+++ pydjango/django_app/bookmark/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from bookmark.models import Bookmark
+
+
+# ListView (Generic View)
+class BookmarkLV(ListView):
+    model = Bookmark
+
+# DetailView (Geeric View)
+class BookmarkDV(DetailView):
+    model = Bookmark
