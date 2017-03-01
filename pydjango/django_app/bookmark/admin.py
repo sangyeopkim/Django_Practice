1593b8b0d5559435b1bedd0986bd24e368821694
sangyeopkim <yeop123456@gmail.com>
2017-03-01 16:33:12 +0900

    주석 수정

--- pydjango/django_app/bookmark/admin.py
+++ pydjango/django_app/bookmark/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from bookmark.models import Bookmark
 
+
 # Admin 사이트에서 Bookmark 내용을 보여주는 클래스
 class BookmarkAdmin(admin.ModelAdmin):
     # title, url 을 화면에 출력
     list_display = ('title', 'url')
+
 
 # admin.site.register() 함수를 사용해 Bookmark, BookmarkAdmin 클래스를 등록
 admin.site.register(Bookmark, BookmarkAdmin)
