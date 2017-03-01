1decaebc1f707b4298f8f026db1835ae925b60c4
sangyeopkim <yeop123456@gmail.com>
2017-03-01 14:48:40 +0900

    Bookmark 기본 테이블 생성

--- pydjango/django_app/bookmark/models.py
+++ pydjango/django_app/bookmark/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    url = models.URLField('url', unique=True)
+
+    # 객체를 문자열로 표현할 때 사용하는 함수
+    # 이 함수를 정의하지 않으면, Admin 사이트나 장고 셸 등에서 테이블명이 제대로 표현되지 않는다.
+    def __str__(self):
+        return self.title
