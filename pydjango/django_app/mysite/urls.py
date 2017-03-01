87e9bdafcaa00d9b3bbe204620fb2d0573dc3245
sangyeopkim <yeop123456@gmail.com>
2017-03-01 16:34:44 +0900

    오타 수정

--- pydjango/django_app/mysite/urls.py
+++ pydjango/django_app/mysite/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import ListView, DetailView
 
-from bookmark.views import BookmarkLV, BookmarkDV
+from bookmark.models import Bookmark
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
-    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail')
+    url(r'^bookmark/$', ListView.as_view(model=Bookmark), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', DetailView.as_view(model=Bookmark), name='detail')
 ]
